36eca95531fc02c9289323d5f65fca05a25e9da0
Douglas Wenzel <115610413+douglaswenzel@users.noreply.github.com>
2024-12-09 13:27:19 -0300

    Atualização Classe: Historico

--- src/DAO/Historico.java
+++ src/DAO/Historico.java
@@ -10,46 +10,46 @@ package DAO;
  */
 public class Historico {
     private String tableName = "Historicos";
-    private int id_his;
-    private String historico;
+    private int ID_HIS;
+    private String DES_HIS;
 
     public Historico() {
         
     }
 
     public Historico(int id_his, String historico) {
-        this.id_his = id_his;
-        this.historico = historico;
+        this.ID_HIS = ID_HIS;
+        this.DES_HIS = DES_HIS;
     }
 
     private boolean validaId_his(int id_his) {
         return id_his > 0;
     }
 
-    private boolean validaHistorico(String historico) {
-        return historico != null && historico.trim().length() > 0;
+    private boolean validaHistorico(String DES_HIS) {
+        return DES_HIS != null && DES_HIS.trim().length() > 0;
     }
 
     public int getId_his() {
-        return id_his;
+        return ID_HIS;
     }
 
     public void setId_his(int id_his) {
         if (!validaId_his(id_his)) {
             throw new IllegalArgumentException("ID histórico inválido");
         }
-        this.id_his = id_his;
+        this.ID_HIS = ID_HIS;
     }
 
     public String getHistorico() {
-        return historico;
+        return DES_HIS;
     }
 
-    public void setHistorico(String historico) {
-        if (!validaHistorico(historico)) {
+    public void setHistorico(String DES_HIS) {
+        if (!validaHistorico(DES_HIS)) {
             throw new IllegalArgumentException("Histórico inválido");
         }
-        this.historico = historico;
+        this.DES_HIS = DES_HIS;
     }
     
     public String getTableName() {
